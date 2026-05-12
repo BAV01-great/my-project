f2b48d8b6f08964da91341e861054b3e46fb56d9
BAVO1 <bamidelemoyinoluwa42@gmail.com>
2026-05-12 18:55:27 +0100

    updated code

--- python.py
+++ python.py
@@ -1,1 +1,23 @@
-print('Hello World')
+Temperature = ''
+while True:
+    value = input('What is your temperature: ')
+    if value == 'quit':
+        print('Program ends')
+        break
+    Temperature = int(value)
+    if Temperature <= 0:
+        print(float(Temperature))
+        print('Freezing')
+    elif  1<= Temperature <=15:
+        print(float(Temperature))
+        print('Cold')
+    elif  16 <= Temperature <=25:
+        print(float(Temperature))
+        print('Warm')
+    elif 26<= Temperature <=35:
+        print(float(Temperature))
+        print('Hot')
+    else:
+        print(float(Temperature))
+        print('Dangerous')
+
